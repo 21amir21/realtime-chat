90f2f0a66b79cc0c8f9d74839d118351abc9444b
Amir <amirosama21@gmail.com>
2025-08-19 02:19:08 +0300

    Fix Dockerfile and configuration for development environment

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :realtime_chat, RealtimeChat.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "realtime_chat_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
